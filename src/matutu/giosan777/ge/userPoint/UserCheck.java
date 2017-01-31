253f096fde083d3e28d45078df00b9152d5b2d5b
giosan777 <giosan777@gmail.com>
2017-02-01 00:33:58 +0400

    davamtavre shemocmeba useris

--- src/matutu/giosan777/ge/userPoint/UserCheck.java
+++ src/matutu/giosan777/ge/userPoint/UserCheck.java
@@ -5,18 +5,13 @@
  */
 package matutu.giosan777.ge.userPoint;
 
-import matutu.giosan777.ge.DAO.Impl.UserDaoImpl;
 import matutu.giosan777.ge.entity.UsersEntity;
 import matutu.giosan777.ge.factory.FactoryDAO;
-import matutu.giosan777.ge.persistence.HibernateUtil;
-import org.hibernate.Session;
-
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 @Path("user")
 public class UserCheck {
@@ -26,23 +21,31 @@ public class UserCheck {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public String momeString(@QueryParam("mail")
+    @Path("/getUser")
+    public String getUser(@QueryParam("mail")
                              @DefaultValue("null")
                                      String mail,
                              @QueryParam("pass")
                              @DefaultValue("0")
                                      String pass) {
-        Collection collection = FactoryDAO.getInstance().getUserDao().getAllUser();
-        Iterator iterator = collection.iterator();
+        Collection collection;
+        if (!mail.equals("nuul") && !(pass.length() < 6)) {
 
-        StringBuilder stringBuilder = new StringBuilder();
-        while (iterator.hasNext()) {
-            UsersEntity usersEntity =(UsersEntity) iterator.next();
-            stringBuilder.append(usersEntity.getMail() + " da kido " + usersEntity.getPass());
+            collection = FactoryDAO.getInstance().getUserDao().getAllUser();
+            Iterator iterator = collection.iterator();
+            while (iterator.hasNext()) {
+                UsersEntity usersEntity =(UsersEntity) iterator.next();
+                if (usersEntity.getMail().equals(mail) && usersEntity.getPass().equals(pass)) {
+                    return "1";
+                }
+            }
+
         }
 
-        return stringBuilder.toString();
+        return "null";
     }
+
+
 
 }
 /*
